eda15015112d6091a20daa363999e75dbec66b45
Stef De Jonghe <stef.dejonghe@azstlucas.be>
2023-11-24 09:54:49 +0100

    add start of prisma crud + display tasks

--- app/tasks/page.js
+++ app/tasks/page.js
@@ -1,7 +1,12 @@
+import TaskForm from "@/components/TaskForm";
+import TaskList from "@/components/TaskList";
+
 const TasksPage = () => {
   return (
-    <div>
+    <div className="max-w-lg">
       <h1 className="text-7xl">TasksPage</h1>
+      <TaskForm />
+      <TaskList />
     </div>
   );
 };
