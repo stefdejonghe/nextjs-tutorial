44a631a678d98ff62a16500fdbc927a43a875de7
Stef De Jonghe <stef.dejonghe@azstlucas.be>
2023-11-27 13:20:29 +0100

    update delete button to use pending state

--- components/DeleteForm.jsx
+++ components/DeleteForm.jsx
@@ -1,10 +1,22 @@
+"use client";
+
 import { deleteTask } from "@/utils/actions";
+import { useFormStatus } from "react-dom";
+
+const SubmitButton = () => {
+  const { pending } = useFormStatus();
+  return (
+    <button className="btn btn-xs btn-error" disabled={pending}>
+      {pending ? "deleting..." : "delete"}
+    </button>
+  );
+};
 
 const DeleteForm = ({ id }) => {
   return (
     <form action={deleteTask}>
       <input type="hidden" name="id" value={id} />
-      <button className="btn btn-xs btn-error">delete</button>
+      <SubmitButton />
     </form>
   );
 };
